c838f77d856fdf5868f05f0caea579b2e3e6b640
David Perez <davidpp80.dpp@gmail.com>
2022-04-18 20:45:25 +0200

    segundo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SidenavComponent } from './parts/sidenav/sidenav.component';
+
+// COMPONENTES
+import { FooterComponent } from './parts/footer/footer.component';
+
+
+// ANGULAR MATERIAL
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/parts/sidenav/sidenav.component.ts
@@ -0,0 +1,39 @@
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+
+@Component({
+  selector: 'app-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['./sidenav.component.css']
+})
+export class SidenavComponent implements OnInit {
+
+  mobileQuery: MediaQueryList;
+
+  fillerNav = [
+    {name:"Home", route:"", icon:"home"},
+    {name:"Análisis de Valores", route:"analisis-valor", icon:"analytics"},
+    {name:"Cartera", route:"cartera", icon:"account_balance"},
+    {name:"Criptomonedas", route:"criptomonedas", icon:"currency_bitcoin"},
+    {name:"Recursos", route:"recursos", icon:"book"},
+    {name:"Contacto", route:"contacto", icon:"contact_mail"}
+  ]
+
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
+  shouldRun = true;
+
+  ngOnInit(): void {
+  }
+
+}
