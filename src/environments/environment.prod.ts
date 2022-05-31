0d1d1ac81be974d6813893f525d5f65052c1d3e9
David Perez <davidpp80.dpp@gmail.com>
2022-05-31 16:10:36 +0200

    mejora blog 3.1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCjyU5pfHADj18qt-fZMUtWVRuc-DDZcTw",
+    authDomain: "proyectoangular-1d854.firebaseapp.com",
+    databaseURL: "https://proyectoangular-1d854-default-rtdb.firebaseio.com",
+    projectId: "proyectoangular-1d854",
+    storageBucket: "proyectoangular-1d854.appspot.com",
+    messagingSenderId: "165554926955",
+    appId: "1:165554926955:web:be226b06449ea6f6b0482e",
+    url: "https://proyectoangular-1d854-default-rtdb.firebaseio.com/"
+  }
 };
